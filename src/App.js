02f6d9dc5111e5994212acb7b97d92cdcdae8551
CodingMirec <m.sykora01@gmail.com>
2021-02-06 21:01:30 +0100

    sidebar prototype done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import Home from "./pages/Home";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="app">
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/login">
+            <Home />
+          </Route>
+          <Route path="/signup">
+            <Home />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,26 +1,61 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import TimerIcon from "@material-ui/icons/Timer";
+import DescriptionIcon from "@material-ui/icons/Description";
+import GroupIcon from "@material-ui/icons/Group";
+import SettingsIcon from "@material-ui/icons/Settings";
+import TodayIcon from "@material-ui/icons/Today";
+import EqualizerIcon from "@material-ui/icons/Equalizer";
 
 import "./Sidebar.css";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar__logo">
-        <h1>Tracker</h1>
-      </div>
+      <Link to="/" className="sidebar__logo">
+        <div>
+          <span>TRACKER</span>
+        </div>
+      </Link>
+      <hr className="sidebar__line" />
       <div className="sidebar__nav">
-        <div className="sidebar__link">
-          <h2>Time Tracker</h2>
-        </div>
-        <div className="sidebar__link">
-          <h2>Projects</h2>
-        </div>
-        <div className="sidebar__link">
-          <h2>Teams</h2>
-        </div>
-        <div className="sidebar__link">
-          <h2>Settings</h2>
-        </div>
+        <Link to="/" className="sidebar__links">
+          <div className="link__option">
+            <TimerIcon />
+            <span>Time Tracker</span>
+          </div>
+        </Link>
+        <Link to="/" className="sidebar__links">
+          <div className="link__option">
+            <TodayIcon />
+            <span>Calendar</span>
+          </div>
+        </Link>
+        <Link to="/" className="sidebar__links">
+          <div className="link__option">
+            <EqualizerIcon />
+            <span>Reports</span>
+          </div>
+        </Link>
+        <Link to="/projects" className="sidebar__links">
+          <div className="link__option">
+            <DescriptionIcon />
+            <span>Projects</span>
+          </div>
+        </Link>
+        <Link to="/teams" className="sidebar__links">
+          <div className="link__option">
+            <GroupIcon />
+            <span>Team</span>
+          </div>
+        </Link>
+        <Link to="/settings" className="sidebar__links">
+          <div className="link__option">
+            <SettingsIcon />
+            <span>Settings</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
