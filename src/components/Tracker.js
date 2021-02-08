6fd12d29df0fffdcddcca3659ebca545f8e3fcb0
CodingMirec <m.sykora01@gmail.com>
2021-02-08 20:35:03 +0100

    top

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -2,12 +2,32 @@ import React from "react";
 
 import "./Tracker.css";
 
+import ScheduleIcon from "@material-ui/icons/Schedule";
+import DehazeIcon from "@material-ui/icons/Dehaze";
+
 function Tracker() {
   return (
     <div className="tracker">
       <div className="container">
         <div className="tracker__top">
-          <h3>What are you working on?</h3>
+          <div className="tracker__left">
+            <input
+              className="tracker__input"
+              type="text"
+              placeholder="What are you working on?"
+            />
+          </div>
+          <div className="tracker__right">
+            <div className="tracker__projectInfo">
+              <button className="tracker__addProject">Add Project</button>
+              <span className="tracker__time">00:00:00</span>
+              <button className="tracker__button">Add Task</button>
+            </div>
+            <div className="tracker__projectSettings">
+              <ScheduleIcon />
+              <DehazeIcon />
+            </div>
+          </div>
         </div>
         <div className="tracker__week">
           <div className="tracker__day">
