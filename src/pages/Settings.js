a45ad10c7c1b82e9bc5351170af68be3cd25ca87
CodingMirec <m.sykora01@gmail.com>
2021-03-08 23:57:17 +0100

    user info

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,18 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 import Sidebar from "../components/Sidebar";
 
 import "./Settings.css";
 
 function Settings() {
+  const history = useHistory();
+
+  const logOut = (e) => {
+    e.preventDefault();
+
+    // some logout magic
+    history.push("/login");
+  };
+
   return (
     <div className="settings">
       <Sidebar />
-      <Link to="/login">logout</Link>
-      <div className="settings__side">
-        <div className="account__info">
-          <div className="account__photo"></div>
+      <div className="settings__container">
+        <div className="user__info">
+          <img
+            className="user__photo"
+            src="https://avatars.dicebear.com/api/avataaars/.svg"
+            alt="avatar"
+          />
+          <div className="user__details">
+            <h2 className="user__name">Miroslav Sykora</h2>
+            <span className="user__mail">some@mail.com</span>
+          </div>
+
+          <button className="logOut" onClick={logOut}>
+            Log Out
+          </button>
         </div>
         <div className="account__settings">some acc settings</div>
       </div>
